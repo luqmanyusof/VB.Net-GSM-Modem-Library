443a3e4287c20e00f1aea336df31271101d62735
locxtronic <lockman_akim@yahoo.com>
2015-11-07 12:34:27 +0800

    First Commit - Initialize

---
+++ GSM Modem Library/GSMModem.vb
@@ -0,0 +1,8 @@
+﻿Imports System.IO.Ports
+Public Class GSMModem
+    Private theModem As New SerialPort
+    Public Sub New(ByVal portName As String, ByVal baud As Integer)
+        theModem.PortName = portName
+        theModem.BaudRate = baud
+    End Sub
+End Class
